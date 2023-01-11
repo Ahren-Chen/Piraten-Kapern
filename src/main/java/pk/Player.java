5d35b4577815ad8e69b7a4bcc15a1ceb67a05c81
Ahren Chen <ahren@hotmail.ca>
2023-01-11 13:14:01 -0500

    Created a base for the player class that will be responsible for playing a singular game, and also completed the calculating score method in the player class based on the 8 dice the player ends up with at the end.

--- src/main/java/pk/Player.java
+++ src/main/java/pk/Player.java
@@ -1,2 +1,37 @@
-package pk;public class Player {
+package pk;
+
+import java.util.Random;
+
+public class Player {
+    private int play() {
+        Dice myDice = new Dice();
+        Faces[] rolledDice = myDice.roll8();
+        while (true) {
+            int skullCount = 0;
+            for (int roll = 0; roll < 8; roll++) {
+                if (rolledDice[roll] == Faces.SKULL) {
+                    skullCount++;
+                }
+            }
+            if (skullCount == 3) {
+                break;
+            }
+
+            for (int roll = 0; roll < 8; roll++) {
+                //randomize rolls and which one to keep
+            }
+        }
+
+        return calculateScore(rolledDice);
+    }
+
+    private static int calculateScore(Faces[] rolledDice) {
+        int score = 0;
+        for (Faces roll: rolledDice) {
+            if (roll == Faces.DIAMOND || roll == Faces.GOLD) {
+                score += 100;
+            }
+        }
+        return score;
+    }
 }
